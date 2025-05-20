77eb1d243586330cd251161a9ba3509b8e8ec4d1
Lucas <lubertanhe@gmail.com>
2025-05-20 17:43:32 -0300

    Fix: Erros Total

--- FeedHub/app/page5/page.tsx
+++
@@ -1,9 +0,0 @@
-import FeedbackForm from "../../components/feedback-form"
-
-export default function Home() {
-  return (
-    <div className="min-h-screen bg-gradient-to-b from-sky-50 to-sky-100 flex items-center justify-center p-4">
-      <FeedbackForm />
-    </div>
-  )
-}
--- feedback-api/doc/intro.md
+++
@@ -1,3 +0,0 @@
-# Introduction to feedback-api
-
-TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
